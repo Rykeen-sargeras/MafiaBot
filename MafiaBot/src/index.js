3885e03c7dc321d66d7eae007f11e6333208d14a
Rykeen-sargeras <dstayt@gmail.com>
2026-08-08 18:52:35 -0400

    Enable global Misfit Mafia branding wrapper

--- MafiaBot/src/index.js
+++ MafiaBot/src/index.js
@@ -1,4 +1,4 @@
-import app from './admin-wrapper.js';
+import app from './brand-wrapper.js';
 import { config } from './config.js';
 import { db, ensureSchema } from './db.js';
 import { startDiscord } from './discord.js';
