88a89cbf60045a554822a09dfafbf8e3b1c160f9
Rykeen-sargeras <dstayt@gmail.com>
2026-08-08 18:44:37 -0400

    Route main site through website admin wrapper

--- MafiaBot/src/index.js
+++ MafiaBot/src/index.js
@@ -1,4 +1,4 @@
-import app from './shop-wrapper.js';
+import app from './admin-wrapper.js';
 import { config } from './config.js';
 import { db, ensureSchema } from './db.js';
 import { startDiscord } from './discord.js';
